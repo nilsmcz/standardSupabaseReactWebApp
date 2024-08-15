5a377b2b5e81ffaf7304c09a87f6a166e76a71f8
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-08-15 19:47:27 +0200

    chrome dev extension

--- app/src/redux/store.js
+++ app/src/redux/store.js
@@ -1,10 +1,9 @@
-import { createStore, combineReducers } from 'redux';
-import userReducer from './reducers/userReducer';
+import { createStore } from 'redux';
+import rootReducer from './reducers'; // Ersetzen Sie dies durch den tatsächlichen Pfad zu Ihrem Root-Reducer
 
-const rootReducer = combineReducers({
-  user: userReducer
-});
-
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
