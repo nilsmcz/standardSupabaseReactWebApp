c678f57d788bf7cfd1c5b5d248d042b67eaf1a87
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-08-20 16:06:36 +0200

    navigation at Home screen

--- app/src/screens/home/Home.jsx
+++ app/src/screens/home/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { logout } from '../../sideEffects/authEffects'
+import { useTranslation } from 'react-i18next';
 
 export default function Home() {
+    const { t } = useTranslation();
+
     return (
-        <div onClick={() => logout()}>Home</div>
+        <>
+            <h1>Home</h1>
+            <div onClick={()=>logout()}>{t("logout")}</div>
+            <Link to="/change-email">{t("change_email")}</Link>
+        </>
     )
 }
