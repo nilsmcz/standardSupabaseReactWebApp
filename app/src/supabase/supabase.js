64df6c2c3144d937917fb4804e6d3eae482490b0
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-08-22 18:14:48 +0200

    online and local supabase setup

--- app/src/supabase/supabase.js
+++ app/src/supabase/supabase.js
@@ -1,6 +1,17 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const isLocal = process.env.SUPABASE_LOCAL === 'true' || false
+
+let supabaseUrl = ''
+let supabaseKey = ''
+
+if (isLocal) {
+    supabaseUrl = 'http://127.0.0.1:54321'
+    supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY_LOCAL
+}
+else {
+    supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+    supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
