a41ad81a121242f8089fe3f2404d238a0e722510
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-09-02 20:09:26 +0200

    stuff

--- app/src/screens/edgeFunctions/EdgeFunctions.jsx
+++ app/src/screens/edgeFunctions/EdgeFunctions.jsx
@@ -1,43 +1,51 @@
 import React, { useState } from 'react';
 import { supabase } from '../../supabase/supabase';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 
 export default function EdgeFunctions() {
     const { t } = useTranslation();
+    const session = useSelector(state => state.auth.session);
+    const accessToken = session?.accessToken;
 
     async function callHello() {
         const { data, error } = await supabase.functions.invoke('hello', {
             body: JSON.stringify({ param1: 5, param2: 10 }),
-        })
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
 
         if (error) {
-            console.error('Error:', error)
+            console.error('Error:', error);
         } else {
-            console.log('Function result:', data.result)
+            console.log('Function result:', data.result);
         }
     }
 
     async function registerUserWithProfile() {
+        console.log("Access Token:", accessToken);
         const { data, error } = await supabase.functions.invoke('registerUSerWithProfile', {
             body: JSON.stringify({ param1: 5, param2: 10 }),
-        })
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
 
         if (error) {
-            console.error('Error:', error)
-        }
-        else {
-            console.log('Function result:', data.result)
+            console.error('Error:', error);
+        } else {
+            console.log('Function result:', data.result);
         }
     }
 
     return (
         <div style={styles.container}>
             <h1>{t('edge_functions')}</h1>
-
             <button onClick={() => callHello()}>{t('test_edge_function')}: hello</button>
             <button onClick={() => registerUserWithProfile()}>{t('test_edge_function')}: registerUserWithProfile</button>
         </div>
-    )
+    );
 }
 
 const styles = {
--- supabase/functions/registerUserWithProfile/index.ts
+++ supabase/functions/registerUserWithProfile/index.ts
@@ -5,42 +5,42 @@ import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { corsHeaders } from "../_shared/cors.ts";
 
 Deno.serve(async (req: Request) => {
+  if (req.method === "OPTIONS") {
+    return new Response(null, { headers: corsHeaders });
+  }
+
   try {
     const supabaseClient = createClient(
       Deno.env.get("SUPABASE_URL") ?? "",
       Deno.env.get("SUPABASE_ANON_KEY") ?? "",
     );
 
-    console.log("supabaseClient:", supabaseClient);
-    console.log("req:", req);
-    console.log("req.headers:", req.headers);
-
     // Get the session or user object
     const authHeader = req.headers.get("Authorization")!;
-    console.log("authHeader:", authHeader);
-    const token = authHeader.replace("Bearer", "");
-    // const { data } = await supabaseClient.auth.getUser(token);
-    // const user = data.user;
+    const token = authHeader.replace("Bearer ", "").trim();
 
     // Benutzer-Informationen abrufen
-    // const { data: authData, error: authError } = await supabaseClient.auth
-    //   .getUser(
-    //     token,
-    //   );
-    // if (authError || !authData.user) {
-    //   throw authError || new Error("Failed to retrieve user");
-    // }
+    const { data: authData, error: authError } = await supabaseClient.auth
+      .getUser(
+        token,
+      );
+    if (authError || !authData.user) {
+      throw authError || new Error("Failed to retrieve user");
+    }
 
-    // const userId = authData.user.id;
+    console.log("authData", authData);
+    const userId = authData.user.id;
+    console.log("userId", userId);
 
     // Leeren Eintrag in der profiles-Tabelle erstellen mit Row Level Security (RLS)
-    // const { data: profileData, error: profileError } = await supabaseClient
-    //   .from("profiles")
-    //   .insert([{ user_id: userId }]);
+    const { data: profileData, error: profileError } = await supabaseClient
+      .from("profiles")
+      .insert([{ user_id: userId }]);
 
-    // if (profileError) {
-    //   throw profileError;
-    // }
+    if (profileError) {
+      console.error("profileError", profileError);
+      throw profileError;
+    }
 
     // Erfolgsmeldung zurückgeben
     // return new Response(JSON.stringify({ success: true, profileData }), {
